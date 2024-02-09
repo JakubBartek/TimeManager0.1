7c2898c0367616c64230aac115d5e59a15000ffb
Jakub Bartek <jakub.bartek145@gmail.com>
2024-02-09 23:01:22 +0100

    Trying app on linux, so far working fine

--- TimeManager0.1/obj/Debug/net8.0/StudyClock0.1.AssemblyInfo.cs
+++ TimeManager0.1/obj/Debug/net8.0/StudyClock0.1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudyClock0.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b58cf240e93d2962a6317897827b74b1229330b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ec148369e7340d25ee81f138f383299bf91567b")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudyClock0.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudyClock0.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
