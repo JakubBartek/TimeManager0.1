247a226637f94fa88dac3170f91d706067195936
Jakub Bartek <jakub.bartek145@gmail.com>
2024-02-10 12:26:22 +0100

    After 3 hours of work, dotet works on my popOs xdddddd fun

--- TimeManager0.1/obj/Debug/net8.0/StudyClock0.1.AssemblyInfo.cs
+++ TimeManager0.1/obj/Debug/net8.0/StudyClock0.1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudyClock0.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ec148369e7340d25ee81f138f383299bf91567b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c2898c0367616c64230aac115d5e59a15000ffb")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudyClock0.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudyClock0.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
